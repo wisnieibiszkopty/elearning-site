05f346fd3037a75dddbf99e4e2a839a3135420ca
Kamilen <s97766@pollub.edu.pl>
2023-12-09 19:11:48 +0100

    setup mary component library

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jazda</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>Siemano mordeczki, z fartem :'DDDD</h1>
+    <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Hello there</h1>
+            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <a href="/login"><button class="btn btn-primary">Login</button></a>
+            <a href="/register"><button class="btn btn-outline btn-primary">Sign up</button></a>
+        </div>
+    </div>
+    </div>
 </body>
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/**/*.{js,blade.php}",
+    "./app/View/Components/**/**/*.php",
+    "./app/Livewire/**/**/*.php",
+
+    // Add mary
+    "./vendor/robsontenorio/mary/src/View/Components/**/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
